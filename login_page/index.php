75a7e9ed4f38761ade7b172b83d341b923ea5b8f
Nagineni Haneel Kumar <116477251+the-lost-sole@users.noreply.github.com>
2022-11-28 18:51:37 +0530

    Updated the Login-page withut signup system

---
+++ login_page/index.php
@@ -0,0 +1,29 @@
+ <!-- https://www.youtube.com/watch?v=JDn6OAMnJwQ&list=PL2WFgdVk-usHGkndjUHy646m0vHi6aO3J -->
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LOGIN</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+     <form action="login.php" method="post">
+     	<h2>LOGIN</h2>
+     	<?php if (isset($_GET['error'])) { ?>
+     		<p class="error"><?php echo $_GET['error']; ?></p>
+     	<?php } ?>
+     	<label>User Name</label>
+     	<input type="text" name="uname" placeholder="User Name"><br>
+
+     	<label>User Name</label>
+     	<input type="password" name="password" placeholder="Password"><br>
+
+     	<button type="submit">Login</button>
+     </form>
+</body>
+</html>	
+
+<html>
+	
+</html>
